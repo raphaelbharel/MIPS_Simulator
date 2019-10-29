fdb66c0cc3c5599ecdf8e17ead6919a7a143c638
daryllimyt <daryllimyt@gmail.com>
2019-10-29 22:38:08 +0000

    test

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-	/* argc (ARGument Count) is int and stores number of command-line arguments passed by the user including the name of the program. So if we pass a value to a program, value of argc would be 2 (one for argument and one for program name)
+/* argc (ARGument Count) is int and stores number of command-line arguments passed by the user including the name of the program. So if we pass a value to a program, value of argc would be 2 (one for argument and one for program name)
 	The value of argc should be non negative.
 	argv(ARGument Vector) is array of character pointers listing all the arguments.
 	If argc is greater than zero,the array elements from argv[0] to argv[argc-1] will contain pointers to strings.
@@ -28,15 +28,16 @@ int main(int argc /* argument count */, char *argv[] /* argument list */)
 {
 	if (argc != 2)
 	{
-		cerr << "Incorrect number of arguments." << "\n";
+		cerr << "Incorrect number of arguments."
+			 << "\n";
 		return 1;
 	}
 
 	string binName = argv[1]; // See above
 
-	cerr << "Binary File name: " << binName << endl;		// For testing/debugging
-	ifstream binStream;										// Create binary stream object
-	binStream.open(binName, ios::binary | ios::in); // Load .bin file as a binary file
+	cerr << "Binary File name: " << binName << endl; // For testing/debugging
+	ifstream binStream;								 // Create binary stream object
+	binStream.open(binName, ios::binary | ios::in);  // Load .bin file as a binary file
 	binStream.seekg(0, ios::end);
 	const streamsize BUFFER_SIZE = binStream.tellg();
 	binStream.seekg(0, ios::beg);
@@ -46,23 +47,24 @@ int main(int argc /* argument count */, char *argv[] /* argument list */)
 	{
 		cerr << "> Successful writing to buffer" << endl;
 		cout << "Streamsize: " << BUFFER_SIZE << endl;
-		cout << "Buffer size: "<< buffer.size() << endl;
+		cout << "Buffer size: " << buffer.size() << endl;
 		cout << "Chars read: " << binStream.gcount() << endl;
 	}
 	binStream.close();
 
 	//Printing all of instruction memory, from first register to last
-	cerr << "Printing buffer string:"
-	for(int i = 0; i<buffer.size(); i++) {
+	cerr << "Printing buffer string:" for (int i = 0; i < buffer.size(); i++)
+	{
 		cout << buffer[i] << " ";
 	}
 	cout << "\n";
 
 	vector<u_int32_t> imem;
-	for (char &c : buffer) {
+	for (char &c : buffer)
+	{
 		imem.push_back(static_cast<byte>(c));
 	}
-
+	// This is a test piece of code from the develop branch!
 
 	return 0;
 }
