74e57036f02e0defae3179dfb2c73b370999a79f
daryllimyt <daryllimyt@gmail.com>
2019-11-16 22:26:18 +0000

    test

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -21,7 +21,7 @@ int main(int argc /* argument count */, char *argv[] /* argument list */)
 	}
 
 	string binName = argv[1]; // Reading second argument from command line
-	ifstream binStream;								// Create binary stream object
+	ifstream binStream;		  // Create binary stream object
 
 	binStream.open(binName, ios::binary); // Load .bin file as a binary file
 	if (!binStream.is_open())
@@ -30,9 +30,9 @@ int main(int argc /* argument count */, char *argv[] /* argument list */)
 		return 1;
 	}
 
-	char buffer[4]; //to hold 4 bytes
+	char buffer[4];				//to hold 4 bytes
 	vector<MEM_TYPE> mem_block; // vector of uint32_t holding memory
-	
+
 	mem_block.resize(MEM_SIZE); //or , 0xFC000000
 
 	// Fill instruction memory
@@ -42,13 +42,9 @@ int main(int argc /* argument count */, char *argv[] /* argument list */)
 		binStream.read(buffer, 4);
 		binStream.peek(); // Check for eof on the next byte
 
-		MEM_TYPE binNo = (static_cast<unsigned char>(buffer[0]) << 24) 
-            | (static_cast<unsigned char>(buffer[1]) << 16) 
-            | (static_cast<unsigned char>(buffer[2]) << 8) 
-            | static_cast<unsigned char>(buffer[3]);
+		MEM_TYPE binNo = (static_cast<unsigned char>(buffer[0]) << 24) | (static_cast<unsigned char>(buffer[1]) << 16) | (static_cast<unsigned char>(buffer[2]) << 8) | static_cast<unsigned char>(buffer[3]);
 
 		cerr << hex << binNo << endl;
-
 
 		mem_block[address] = binNo;
 		address += 1;
@@ -64,18 +60,17 @@ int main(int argc /* argument count */, char *argv[] /* argument list */)
 	int executions = 0;
 	ADDR_TYPE next_instruction;
 
-
-	for (; C.pc>=ADDR_INSTR_OFFSET &&C.pc < ADDR_INSTR_OFFSET + ADDR_INSTR_LENGTH; /*&& (mem_block[C.pc] != 0); address++*/)
+	for (; C.pc >= ADDR_INSTR_OFFSET && C.pc < ADDR_INSTR_OFFSET + ADDR_INSTR_LENGTH; /*&& (mem_block[C.pc] != 0); address++*/)
 	{
 		C.display();
-		C.reg[0] = 0; // $0 is always 0 on every clock cycle
+		C.reg[0] = 0;			  // $0 is always 0 on every clock cycle
 		next_instruction = C.npc; //The preserved next instruction enables a branch delay
-		
+
 		C.instr = mem_block[C.pc];
-		if (!C.instr) // If instruction is just 0 ie empty
-		{
-			break;
-		}
+		// if (!C.instr) // If instruction is just 0 ie empty
+		// {
+		// 	break;
+		// }
 
 		char instr_type = read_instruction(C.instr);
 
@@ -103,13 +98,13 @@ int main(int argc /* argument count */, char *argv[] /* argument list */)
 	//__print_memory(mem_block);
 	//__print_memory_specific(mem_block, ADDR_INSTR_OFFSET, ADDR_INSTR_OFFSET + 10 /*executions*/);
 
-	 //Successful termination/completion = return low 8-bits of the value in register $2.
-    INSTR_TYPE exit_code = C.reg[2] & 0xFF;
-     cout<<"Return code: "<<exit_code<<endl;
-    exit(exit_code);
+	//Successful termination/completion = return low 8-bits of the value in register $2.
+	INSTR_TYPE exit_code = C.reg[2] & 0xFF;
+	cout << "Return code: " << exit_code << endl;
+	exit(exit_code);
 
-	return 0;
-} // END OF MAIN
+	return 0:
+} // END OF	MAIN
 
 // FUNCTION DEFINITIONS
 char read_instruction(INSTR_TYPE &instruction)
