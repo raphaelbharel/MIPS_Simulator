2489dbec8c2ef92c7294adfba587661edc88fc79
daryllimyt <daryllimyt@gmail.com>
2019-11-18 11:47:31 +0000

    implemented additional functions

--- src/i_type_instructions.cpp
+++ src/i_type_instructions.cpp
@@ -32,16 +32,16 @@ int i_type_instructions::execute()
         BNE(C, src1, dest, sx_idata);
         return 1;
     case 0X24:
-        // LBU(C , src1, dest, sx_idata);
+        LBU(C, src1, dest, sx_idata);
         return 1;
     case 0X20:
-        // LB(C , src1, dest, sx_idata);
+        LB(C, src1, dest, sx_idata);
         return 1;
     case 0X25:
-        // LHU(C , src1, dest, sx_idata);
+        LHU(C, src1, dest, sx_idata);
         return 1;
     case 0X21:
-        // LH(C , src1, dest, sx_idata);
+        LH(C, src1, dest, sx_idata);
         return 1;
     case 0XF:
         LUI(C, src1, dest, idata);
@@ -50,10 +50,10 @@ int i_type_instructions::execute()
         LW(C, src1, dest, sx_idata);
         return 1;
     case 0X22:
-        // LWL(C , src1, dest, sx_idata);
+        LWL(C, src1, dest, sx_idata);
         return 1;
     case 0X26:
-        // LWR(C , src1, dest, sx_idata);
+        LWR(C, src1, dest, sx_idata);
         return 1;
     case 0X28:
         // SB(C , src1, dest, sx_idata);
@@ -143,22 +143,149 @@ void i_type_instructions::BNE(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32
         C->npc = C->npc + 1;
     }
 }
-// void i_type_instructions::LBU(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32_t &sx_idata)
-// {
-//     cerr << "LBU" << endl; // Load byte unsigned LBU $1 16bOFFSET($2)
-// }
-// void i_type_instructions::LB(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32_t &sx_idata)
-// {
-//     cerr << "LB" << endl;
-// }
-// void i_type_instructions::LHU(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32_t &sx_idata)
-// {
-//     cerr << "LHU" << endl;
-// }
-// void i_type_instructions::LH(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32_t &sx_idata)
-// {
-//     cerr << "LH" << endl;
-// }
+void i_type_instructions::LBU(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32_t &sx_idata)
+{
+    cerr << "LBU" << endl;                          // Load byte unsigned LBU $1 16bOFFSET($2)
+    int32_t raw_mem_addr = sx_idata + C->reg[src1]; // 16b signed offset + base
+    int32_t mem_addr = raw_mem_addr / 4;
+    if (DEBUG)
+    {
+        cerr << hex << ">> LBU Raw memory address: " << raw_mem_addr << "\n";
+        cerr << hex << ">> LBU Effective memory address: " << mem_addr << "\n";
+    }
+
+    if (mem_addr < 0)
+    {
+        throw(static_cast<int>(MEMORY_EXIT_CODE));
+    }
+    MEM_TYPE word_at_address = (*(C->mem))[mem_addr]; // mem is a pointer to the memory block
+    int byte_offset = raw_mem_addr % 4;
+    switch (byte_offset)
+    {
+    case 0:
+        C->reg[dest] = 0xFF000000 & word_at_address;
+        break;
+    case 1:
+        C->reg[dest] = 0x00FF0000 & word_at_address;
+        break;
+    case 2:
+        C->reg[dest] = 0x0000FF00 & word_at_address;
+        break;
+    case 3:
+        C->reg[dest] = 0x000000FF & word_at_address;
+        break;
+    default:
+        throw(static_cast<int>(MEMORY_EXIT_CODE));
+        break;
+    }
+    C->npc = C->npc + 1;
+}
+void i_type_instructions::LB(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32_t &sx_idata)
+{
+    cerr << "LB" << endl;
+    int32_t raw_mem_addr = sx_idata + C->reg[src1]; // 16b signed offset + base
+    int32_t mem_addr = raw_mem_addr / 4;
+    if (DEBUG)
+    {
+        cerr << hex << ">> LB Raw memory address: " << raw_mem_addr << "\n";
+        cerr << hex << ">> LB Effective memory address: " << mem_addr << "\n";
+    }
+
+    if (mem_addr < 0)
+    {
+        throw(static_cast<int>(MEMORY_EXIT_CODE));
+    }
+    MEM_TYPE word_at_address = (*(C->mem))[mem_addr]; // mem is a pointer to the memory block
+    int byte_offset = raw_mem_addr % 4;
+    MEM_TYPE pre_sx;
+    switch (byte_offset)
+    {
+    case 0:
+        C->reg[dest] = 0xFF000000 & word_at_address; // doesn't need to sign extend
+        break;
+    case 1:
+        pre_sx = 0x00FF0000 & word_at_address;
+        C->reg[dest] = sign_extend_int32(pre_sx, 24);
+        break;
+    case 2:
+        pre_sx = 0x0000FF00 & word_at_address;
+        C->reg[dest] = sign_extend_int32(pre_sx, 16);
+        break;
+    case 3:
+        pre_sx = 0x000000FF & word_at_address;
+        C->reg[dest] = sign_extend_int32(pre_sx, 8);
+        break;
+    default:
+        throw(static_cast<int>(MEMORY_EXIT_CODE));
+        break;
+    }
+
+    C->npc = C->npc + 1;
+}
+void i_type_instructions::LHU(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32_t &sx_idata)
+{
+    cerr << "LHU" << endl;
+    int32_t raw_mem_addr = sx_idata + C->reg[src1]; // 16b signed offset + base
+    int32_t mem_addr = raw_mem_addr / 4;
+    if (DEBUG)
+    {
+        cerr << hex << ">> LBU Raw memory address: " << raw_mem_addr << "\n";
+        cerr << hex << ">> LBU Effective memory address: " << mem_addr << "\n";
+    }
+
+    if ((mem_addr < 0) || (mem_addr % 2 != 0)) // LSB non zero i.e. not multiple of 2
+    {
+        throw(static_cast<int>(MEMORY_EXIT_CODE));
+    }
+    MEM_TYPE word_at_address = (*(C->mem))[mem_addr]; // mem is a pointer to the memory block
+    int byte_offset = raw_mem_addr % 4;
+    switch (byte_offset)
+    {
+    case 0:
+        C->reg[dest] = 0xFFFF0000 & word_at_address;
+        break;
+    case 2:
+        C->reg[dest] = 0x0000FFFF & word_at_address;
+        break;
+    default:
+        throw(static_cast<int>(MEMORY_EXIT_CODE));
+        break;
+    }
+    C->npc = C->npc + 1;
+}
+void i_type_instructions::LH(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32_t &sx_idata)
+{
+    cerr << "LH" << endl;
+    int32_t raw_mem_addr = sx_idata + C->reg[src1]; // 16b signed offset + base
+    int32_t mem_addr = raw_mem_addr / 4;
+    if (DEBUG)
+    {
+        cerr << hex << ">> LH Raw memory address: " << raw_mem_addr << "\n";
+        cerr << hex << ">> LH Effective memory address: " << mem_addr << "\n";
+    }
+
+    if ((mem_addr < 0) || (mem_addr % 2 != 0)) // LSB non zero i.e. not multiple of 2
+    {
+        throw(static_cast<int>(MEMORY_EXIT_CODE));
+    }
+    MEM_TYPE word_at_address = (*(C->mem))[mem_addr]; // mem is a pointer to the memory block
+    int byte_offset = raw_mem_addr % 4;
+    MEM_TYPE pre_sx;
+    switch (byte_offset)
+    {
+    case 0:
+        C->reg[dest] = 0xFFFF0000 & word_at_address;
+        break;
+    case 2:
+        pre_sx = 0x0000FFFF & word_at_address;
+        C->reg[dest] = sign_extend_int32(pre_sx, 16);
+        break;
+    default:
+        throw(static_cast<int>(MEMORY_EXIT_CODE));
+        break;
+    }
+    C->npc = C->npc + 1;
+}
 
 void i_type_instructions::LUI(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, INSTR_TYPE &idata)
 {
@@ -176,14 +303,19 @@ void i_type_instructions::LUI(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, INSTR
 
     C->npc = C->npc + 1;
 }
+
 void i_type_instructions::LW(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32_t &sx_idata)
 {
     cerr << "LW" << endl;
     int32_t raw_mem_addr = sx_idata + C->reg[src1];
     int32_t mem_addr = raw_mem_addr / 4;
-    if (DEBUG) {cerr << hex << ">> LW Effective memory address: " << mem_addr << "\n";}
+    if (DEBUG)
+    {
+        cerr << hex << ">> LW Raw memory address: " << raw_mem_addr << "\n";
+        cerr << hex << ">> LW Effective memory address: " << mem_addr << "\n";
+    }
 
-    if ((mem_addr < 0) || (raw_mem_addr%4 != 0)) // If either of the two LSB of address are non-zero then throw exception
+    if ((mem_addr < 0) || (raw_mem_addr % 4 != 0)) // If either of the two LSB of address are non-zero then throw exception
     {
         throw(static_cast<int>(MEMORY_EXIT_CODE));
     }
@@ -198,14 +330,93 @@ void i_type_instructions::LW(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32_
     }
     C->npc = C->npc + 1;
 }
-// void i_type_instructions::LWL(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32_t &sx_idata)
-// {
-//     cerr << "LWL" << endl;
-// }
-// void i_type_instructions::LWR(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32_t &sx_idata)
-// {
-//     cerr << "LWR" << endl;
-// }
+
+void i_type_instructions::LWL(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32_t &sx_idata)
+{
+    cerr << "LWL" << endl;
+    int32_t raw_mem_addr = sx_idata + C->reg[src1]; // 16b signed offset + base
+    int32_t mem_addr = raw_mem_addr / 4;
+    if (DEBUG)
+    {
+        cerr << hex << ">> LWL Raw memory address: " << raw_mem_addr << "\n";
+        cerr << hex << ">> LWL Effective memory address: " << mem_addr << "\n";
+    }
+
+    if (mem_addr < 0)
+    {
+        throw(static_cast<int>(MEMORY_EXIT_CODE));
+    }
+    MEM_TYPE word_at_address = (*(C->mem))[mem_addr]; // mem is a pointer to the memory block
+    MEM_TYPE MSBytes;
+    int byte_offset = raw_mem_addr % 4;
+    switch (byte_offset)
+    // Extract bytes starting from byte_offset to end of aligned word then left shift
+    {
+    case 0:
+        C->reg[dest] = word_at_address; // Overwrite entire register
+        break;
+    case 1:
+        MSBytes = (0x00FFFFFF & word_at_address) << 8;
+        C->reg[dest] = (C->reg[dest] & 0x000000FF) | MSBytes; // Merge contents
+        break;
+    case 2:
+        MSBytes = (0x0000FFFF & word_at_address) << 16;
+        C->reg[dest] = (C->reg[dest] & 0x0000FFFF) | MSBytes; // Merge contents
+        break;
+    case 3:
+        MSBytes = (0x000000FF & word_at_address) << 24;
+        C->reg[dest] = (C->reg[dest] & 0x00FFFFFF) | MSBytes; // Merge contents
+        break;
+    default:
+        throw(static_cast<int>(MEMORY_EXIT_CODE));
+        break;
+    }
+    C->npc = C->npc + 1;
+}
+
+void i_type_instructions::LWR(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32_t &sx_idata)
+{
+    cerr << "LWR" << endl;
+    int32_t raw_mem_addr = sx_idata + C->reg[src1]; // 16b signed offset + base
+    int32_t mem_addr = raw_mem_addr / 4;
+    if (DEBUG)
+    {
+        cerr << hex << ">> LWR Raw memory address: " << raw_mem_addr << "\n";
+        cerr << hex << ">> LWR Effective memory address: " << mem_addr << "\n";
+    }
+
+    if (mem_addr < 0)
+    {
+        throw(static_cast<int>(MEMORY_EXIT_CODE));
+    }
+    MEM_TYPE word_at_address = (*(C->mem))[mem_addr]; // mem is a pointer to the memory block
+    MEM_TYPE LSBytes;
+    int byte_offset = raw_mem_addr % 4;
+    switch (byte_offset)
+    // Extract bytes starting from byte_offset to end of aligned word then left shift
+    {
+    case 0:
+        LSBytes = (0xFF000000 & word_at_address) >> 24;
+        C->reg[dest] = (C->reg[dest] & 0xFFFFFF00) | LSBytes; // Merge contents
+        break;
+    case 1:
+        LSBytes = (0xFFFF0000 & word_at_address) >> 16;
+        C->reg[dest] = (C->reg[dest] & 0xFFFF0000) | LSBytes; // Merge contents
+        break;
+    case 2:
+        LSBytes = (0xFFFFFF00 & word_at_address) >> 8;
+        C->reg[dest] = (C->reg[dest] & 0xFF000000) | LSBytes; // Merge contents
+        break;
+    case 3:
+        C->reg[dest] = word_at_address; // Overwrite entire register
+        break;
+    default:
+        throw(static_cast<int>(MEMORY_EXIT_CODE));
+        break;
+    }
+    C->npc = C->npc + 1;
+}
+
 void i_type_instructions::ORI(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, INSTR_TYPE &idata)
 {
     cerr << "ORI" << endl;
@@ -239,9 +450,12 @@ void i_type_instructions::SW(CPU *&C, INSTR_TYPE &src1, INSTR_TYPE &dest, int32_
     cerr << "SW" << endl;
     INSTR_TYPE raw_mem_addr = sx_idata + C->reg[src1];
     INSTR_TYPE mem_addr = raw_mem_addr / 4;
-    if (DEBUG) {cerr << hex << ">> SW Effective memory address: " << mem_addr << "\n";}
+    if (DEBUG)
+    {
+        cerr << hex << ">> SW Effective memory address: " << mem_addr << "\n";
+    }
 
-    if ((mem_addr < 0) || (raw_mem_addr%4 != 0)) // If either of the two LSB of address are non-zero then throw exception
+    if ((mem_addr < 0) || (raw_mem_addr % 4 != 0)) // If either of the two LSB of address are non-zero then throw exception
     {
         throw(static_cast<int>(MEMORY_EXIT_CODE));
     }
